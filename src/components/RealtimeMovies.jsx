3977e004f38e69e7eb27b58863aa9320329b98e4
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-22 20:27:24 +0530

    onSnapshot

--- src/components/RealtimeMovies.jsx
+++ src/components/RealtimeMovies.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import { onSnapshot } from "firebase/firestore";
+import { movieCollectionRef } from "../lib/firestore.collections";
 
 export default function RealtimeMovies() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    onSnapshot(movieCollectionRef);
+    return () => {};
+  }, []);
+
   return (
     <div>
-      
+      <h4>RealtimeMovies</h4>
     </div>
-  )
+  );
 }
